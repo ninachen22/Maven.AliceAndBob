5cbe82ec4ff43fbb8718622ed1c4de78c1cc4504
ninachen22 <116048270+ninachen22@users.noreply.github.com>
2022-10-28 19:28:30 -0400

    made changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,20 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args ){
+        System.out.println("Type out your name");
+        Scanner input = new Scanner(System.in);
+        String name = input.nextLine();
+        String name1 = "Alice";
+        String name2 = "Bob";
+        if (name.equals(name1) || name.equals(name2)) {
+            System.out.println("Hi " + name + ", welcome!");
+        } else {
+            System.out.println("Name not found");
+        }
 
     }
 }
